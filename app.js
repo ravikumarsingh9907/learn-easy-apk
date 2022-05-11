7c2b0bc9f8524c58466927da513f89082d226a85
Ravi Kumar <ravikumarsingh9907@gmail.com>
2022-05-11 22:33:56 +0530

    remove secure from session

--- app.js
+++ app.js
@@ -46,7 +46,6 @@ const secretKey = {
   saveUninitialized: true,
   cookie: {
     httpOnly: true,
-    secure: true,
     expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
     maxAge: 1000 * 60 * 60 * 24 * 7,
   },
