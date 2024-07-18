9a3a6fec1bb8f7ea78b9576ef08a5906e9af4d54
Ravi Kumar <ravi.kumar@rapidops.com>
2024-07-18 17:29:07 +0530

    added console in db.

--- db/database.js
+++ db/database.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+console.log(process.env.DB_URL);
 const databaseUrl =
   process.env.DB_URL || "mongodb://localhost:27017/learn-easy";
 
